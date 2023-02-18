2e2b20317c71bb2ad61e0bf767bfe09144b0bb50
Ville Kalliomäki <i@mail.v-k.fi>
2023-02-18 22:13:49 +0200

    Added Fx

--- cmd/runtask/runtask.go
+++ cmd/runtask/runtask.go
@@ -1,12 +1,60 @@
 package main
 
 import (
-	"runtask/internal/config"
-	"runtask/internal/logs"
+	"context"
+	"io"
+	"net"
+	"net/http"
+
+	"go.uber.org/fx"
+	"go.uber.org/fx/fxevent"
+	"go.uber.org/zap"
 )
 
 func main() {
-	config.GenerateConfig()
+	fx.New(fx.WithLogger(func(log *zap.Logger) fxevent.Logger {
+		return &fxevent.ZapLogger{Logger: log}
+	}), fx.Provide(NewHTTPServer, NewServeMux, NewEchoHandler, zap.NewExample), fx.Invoke(func(*http.Server) {})).Run()
+}
 
-	logs.Logger.Info("Starting application")
+func NewHTTPServer(lc fx.Lifecycle, mux *http.ServeMux, log *zap.Logger) *http.Server {
+	srv := &http.Server{Addr: ":8080", Handler: mux}
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			ln, err := net.Listen("tcp", srv.Addr)
+			if err != nil {
+				return err
+			}
+			log.Info("Starting HTTP server", zap.String("addr", srv.Addr))
+			go srv.Serve(ln)
+
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			return srv.Shutdown(ctx)
+		},
+	})
+	return srv
+}
+
+type EchoHandler struct {
+	log *zap.Logger
+}
+
+func NewEchoHandler(log *zap.Logger) *EchoHandler {
+	return &EchoHandler{log: log}
+}
+
+func (h *EchoHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if _, err := io.Copy(w, r.Body); err != nil {
+		h.log.Warn("Failed to handle request", zap.Error(err))
+	}
+}
+
+// NewServeMux builds a ServeMux that will route requests
+// to the given EchoHandler.
+func NewServeMux(echo *EchoHandler) *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.Handle("/echo", echo)
+	return mux
 }
